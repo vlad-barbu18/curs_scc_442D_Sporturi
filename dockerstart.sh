25e52c59ec694139f156a594bb0a35d08036c560
Andrei Taga <tagaandrei2015@gmail.com>
2026-05-12 14:52:39 +0300

    modificari dockerfile, jenkins file, quick requirements, tests si stergere fisiere aplicatie sysinfo

--- dockerstart.sh
+++ dockerstart.sh
@@ -3,6 +3,6 @@ echo "Activare venv:"
 #source .vanv/bin/activate
 . .venv/bin/activate
 echo "Configurare variabila mediu FLASK_APP"
-export FLASK_APP=sysinfo
+export FLASK_APP=sporturi
 echo "Start server:"
 exec flask run -h 0.0.0.0 -p 5011 --reload
