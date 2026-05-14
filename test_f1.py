38abe95225e09307514ec8968affec1c5870c6e9
andreea <stancu_andreea41@yahoo.com>
2026-05-14 16:37:35 +0300

    modif index max

--- test_f1.py
+++ test_f1.py
@@ -3,7 +3,7 @@ from app.lib.f1_logic import primii_trei_piloti, detalii_circuit
 def test_podium_nu_este_gol():
     rezultat = primii_trei_piloti()
     assert len(rezultat) == 3
-    assert rezultat[1] == "Max Verstappen"
+    assert rezultat[0] == "Max Verstappen"
 
 def test_circuit_monaco():
     descriere = detalii_circuit("Monaco")
