2297e0322c14c725cba4458f235b667f48c53293
alexandrabocai <alexandrabocai03@yahoo.com>
2026-05-16 17:56:56 +0300

    Modificare gitignore

---
+++ app/lib/biblioteca_sporturi.py
@@ -0,0 +1,5 @@
+def reguli_volei():
+    return "Voleiul se joacă între două echipe de câte 6 jucători, separate de un fileu."
+
+def echipament_volei():
+    return "Echipamentul pentru volei include tricou, șort, încălțăminte sport, genunchiere și minge de volei."
