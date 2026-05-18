3c6dc13dfd99fa74d931c29a3ee14b0c7d7bac27
Selter Andrei <andrei_selter@yahoo.com>
2026-05-18 16:07:58 +0300

    feat: biblioteca cu functii pentru golf

--- app/lib/biblioteca_sporturi.py
+++ app/lib/biblioteca_sporturi.py
@@ -1,7 +1,7 @@
 
 def reguli_golf():
 
-    return "Reguli generale pentru golf."
+    return "Reguli generale pentru golf.<br>In golf , scopul este sa introduci mingea in fiecare gaura folosind cat mai putine lovituri. Jucatorul loveste mingea cu o crosa de la zona de start, numita tee, pana la zona green. Daca mingea ajunge in bunker, apa sau in afara terenului, se pot aplica penalizari." 
 
 
 def echipament_golf():
