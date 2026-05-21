34c81e628f771e707282f6dcc62e34e5edba6e14
a-blindu <andrei.blindu11@gmail.com>
2026-05-21 22:51:03 +0300

    Update sporturi.py

--- sporturi.py
+++ sporturi.py
@@ -5,7 +5,7 @@ Element ales: badminton.
 
 from flask import Flask, redirect
 
-from app.lib.biblioteca_sporturi import (
+from app.lib.biblioteca_badminton import (
     reguli_badminton,
     echipament_badminton,
 )
